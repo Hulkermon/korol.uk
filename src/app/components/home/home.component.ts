e54b748176d8b434f3769c3a18a2120e9132a866
Josh Koroluk <joshua.koroluk@stud.hslu.ch>
2023-09-11 13:58:43 -0600

    added some cursor-following effects

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  doCssTrickery = false;
+
+  @ViewChild('cursorBackground') cursorBgElRef?: ElementRef;
+  @ViewChild('flippyCard') flippyCardElRef?: ElementRef;
+
+  @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent) {
+    this.updateShadowPos(event.clientX, event.clientY);
+  }
+
+  private updateShadowPos(posX: number, posY: number) {
+    const backgroundSize = 100;
+    if (this.doCssTrickery) {
+      this.cursorBgElRef!.nativeElement.style.display = 'inherit';
+      this.cursorBgElRef!.nativeElement.style.left = `${posX - backgroundSize / 2}px`;
+      this.cursorBgElRef!.nativeElement.style.top = `${posY - backgroundSize / 2}px`;
+    }
+    else {
+      this.cursorBgElRef!.nativeElement.style.display = 'none';
+    }
+  }
+
+  @HostListener('mouseover', ['$event.target']) onHover(hoveringElement: any) {
+    const isHoveringFlippyCard = hoveringElement == this.flippyCardElRef!.nativeElement;
+    this.doCssTrickery = isHoveringFlippyCard;
+  }
+
+  @HostListener('mouseout', ['$event.target']) offHover(hoveredElement: any) {
+    const noLongerHoveringFlippyCard = hoveredElement == this.flippyCardElRef!.nativeElement;
+    this.doCssTrickery = !noLongerHoveringFlippyCard;
+    this.updateShadowPos(0, 0);
+  }
+
+  ngAfterViewInit(): void {
+    if (this.flippyCardElRef === undefined) {
+      throw new Error("cursorBackground element not found.");
+    }
+  }
 }
