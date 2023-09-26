a4ed0a25ff6770859e70fae1cb948d0d3e252d0b
Josh Koroluk <joshua.koroluk@stud.hslu.ch>
2023-09-26 11:38:22 -0600

    removed main content bc I don't like it.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,40 +6,56 @@ import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@
 })
 export class HomeComponent implements AfterViewInit {
 
-  doCssTrickery = false;
+  // doCssTrickery = false;
+  hoveringCard = false;
 
   @ViewChild('cursorBackground') cursorBgElRef?: ElementRef;
-  @ViewChild('flippyCard') flippyCardElRef?: ElementRef;
+  @ViewChild('flippyCard') cardElRef?: ElementRef;
 
   @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent) {
-    this.updateShadowPos(event.clientX, event.clientY);
+    // this.updateShadowPos(event.clientX, event.clientY);
+    this.updateCardShadow(event.clientX, event.clientY);
   }
 
-  private updateShadowPos(posX: number, posY: number) {
-    if (this.doCssTrickery) {
-      this.cursorBgElRef!.nativeElement.style.display = 'inherit';
-      this.cursorBgElRef!.nativeElement.style.left = `${posX}px`;
-      this.cursorBgElRef!.nativeElement.style.top = `${posY}px`;
+  private updateCardShadow(mousePosX: number, mousePosY: number) {
+    if (this.hoveringCard) {
+      const cardEl = (this.cardElRef!.nativeElement as Element);
+      const cardBounds = cardEl.getBoundingClientRect();
+      const cardPosX = (cardBounds.right + cardBounds.left) / 2;
+      const cardPosY = (cardBounds.bottom + cardBounds.top) / 2;
+      const shadowPosX = (cardPosX - mousePosX) / 5;
+      const shadowPosY = (cardPosY - mousePosY) / 5;
+      this.cardElRef!.nativeElement.style.boxShadow = `#ffc0cb67 ${shadowPosX}px ${shadowPosY}px 20px 20px`;
+      this.cardElRef!.nativeElement.style.transition = 'box-shadow 0ms';
+    } else {
+      this.cardElRef!.nativeElement.style.boxShadow = 'none';
+      this.cardElRef!.nativeElement.style.transition = 'box-shadow 350ms ease';
     }
-    else {
-      this.cursorBgElRef!.nativeElement.style.display = 'none';
-    }
   }
 
-  @HostListener('mouseover', ['$event.target']) onHover(hoveringElement: any) {
-    const isHoveringFlippyCard = hoveringElement == this.flippyCardElRef!.nativeElement;
-    this.doCssTrickery = isHoveringFlippyCard;
+  // private updateShadowPos(posX: number, posY: number) {
+  //   if (this.doCssTrickery) {
+  //     this.cursorBgElRef!.nativeElement.style.display = 'inherit';
+  //     this.cursorBgElRef!.nativeElement.style.left = `${posX}px`;
+  //     this.cursorBgElRef!.nativeElement.style.top = `${posY}px`;
+  //   }
+  //   else {
+  //     this.cursorBgElRef!.nativeElement.style.display = 'none';
+  //   }
+  // }
+
+  @HostListener('mouseover', ['$event.target']) onHover(targetElement: any) {
+    this.hoveringCard = targetElement == this.cardElRef!.nativeElement;
   }
 
-  @HostListener('mouseout', ['$event.target']) offHover(hoveredElement: any) {
-    const noLongerHoveringFlippyCard = hoveredElement == this.flippyCardElRef!.nativeElement;
-    this.doCssTrickery = !noLongerHoveringFlippyCard;
-    this.updateShadowPos(0, 0);
-  }
+  // @HostListener('mouseout', ['$event.target']) offHover(hoveredElement: any) {
+  //   this.hoveringCard = hoveredElement == this.cardElRef!.nativeElement;
+  //   this.updateShadowPos(0, 0);
+  // }
 
   ngAfterViewInit(): void {
-    if (this.flippyCardElRef === undefined) {
-      throw new Error("cursorBackground element not found.");
+    if (this.cardElRef === undefined) {
+      throw new Error("card element (#flilppyCard) not found.");
     }
   }
 }
