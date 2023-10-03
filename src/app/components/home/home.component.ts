c06dcffaf9ff046099003601127410d31a71e079
Josh Koroluk <joshua.koroluk@stud.hslu.ch>
2023-10-03 13:11:58 -0600

    added subscribable mousePos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,37 @@
-import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', './home.nav.scss']
 })
-export class HomeComponent {
-  navLinks = [
-    'Home',
-    'Projects',
-  ];
+export class HomeComponent implements OnDestroy {
+  subscriptions: Subscription[] = [];
+
+  navLinks = ['Home', 'Projects'];
+  elementRoot = document.documentElement;
+  mousePos = new Subject<{ x: string; y: string }>();
+
+  constructor() {
+    this.setupScssVariables();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
+
+  private setupScssVariables() {
+    this.getMousePos();
+  }
+
+  private getMousePos() {
+    addEventListener('mousemove', mouseEvent => {
+      this.mousePos.next({
+        x: `${mouseEvent.clientX}px`,
+        y: `${mouseEvent.clientY}px`
+      });
+    });
+  }
 }
