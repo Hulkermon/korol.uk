3985095bb7e3fa3207af10ec6ef1a42766225f33
Josh Koroluk <joshua.koroluk@stud.hslu.ch>
2023-10-03 14:53:59 -0600

    added some fun mouse-based hover effects

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnDestroy } from '@angular/core';
+// prettier-ignore
+import { AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChildren } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss', './home.nav.scss']
 })
-export class HomeComponent implements OnDestroy {
+export class HomeComponent implements OnDestroy, AfterViewInit {
   subscriptions: Subscription[] = [];
 
   navLinks = ['Home', 'Projects'];
+  @ViewChildren('navLink') navLinkElements?: QueryList<ElementRef>;
   elementRoot = document.documentElement;
-  mousePos = new Subject<{ x: string; y: string }>();
+  mousePos = new Subject<{ x: number; y: number }>();
 
-  constructor() {
-    this.setupScssVariables();
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.getMousePos();
+    this.bindMousePosToScss();
+    this.bindNavItemPositionsToScss();
   }
 
   ngOnDestroy(): void {
@@ -22,15 +28,46 @@ export class HomeComponent implements OnDestroy {
     });
   }
 
-  private setupScssVariables() {
-    this.getMousePos();
-  }
-
   private getMousePos() {
     addEventListener('mousemove', mouseEvent => {
       this.mousePos.next({
-        x: `${mouseEvent.clientX}px`,
-        y: `${mouseEvent.clientY}px`
+        x: mouseEvent.clientX,
+        y: mouseEvent.clientY
+      });
+    });
+  }
+
+  private setScssVariable(key: string, value: any): void {
+    this.elementRoot.style.setProperty(`--${key}`, value);
+  }
+
+  private bindMousePosToScss() {
+    const sub = this.mousePos.subscribe(pos => {
+      this.setScssVariable('mouse-x', pos.x);
+      this.setScssVariable('mouse-y', pos.y);
+    });
+    this.subscriptions.push(sub);
+  }
+
+  private bindNavItemPositionsToScss() {
+    let tempMouseSub: Subscription;
+    this.navLinkElements?.forEach((navLink, i) => {
+      //prettier-ignore
+      navLink.nativeElement.addEventListener('mouseover', (mouseEvent: MouseEvent) => {
+        const rect: DOMRect = navLink.nativeElement.getBoundingClientRect();
+        const xCenter = (rect.left + rect.right) / 2;
+        const yCenter = (rect.top + rect.bottom) / 2;
+
+        tempMouseSub = this.mousePos.subscribe(pos => {
+          const xOffsetFromElement = pos.x - xCenter;
+          const yOffsetFromElement = pos.y - yCenter;
+          this.setScssVariable(`mouse-x-from-nav`, xOffsetFromElement);
+          this.setScssVariable(`mouse-y-from-nav`, yOffsetFromElement);
+        })
+      });
+      //prettier-ignore
+      navLink.nativeElement.addEventListener('mouseleave', (mouseEvent: MouseEvent) => {
+        tempMouseSub.unsubscribe();
       });
     });
   }
