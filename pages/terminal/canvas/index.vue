8d2fa619932817b79b40d2585bdca2b88073b7da
josh <hulkermon@gmail.com>
2025-03-27 06:23:49 +0100

    📺✨ summoned CRT terminal UI - it slaps

---
+++ pages/terminal/canvas/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="canvas bg-[#0a0a0a] text-[#33ff00] h-screen">
+    <div class="canvas__header">
+      <h1>Canvas</h1>
+    </div>
+    <div class="canvas__content">
+      <canvas id="myCanvas" width="500" height="500"></canvas>
+    </div>
+  </div>
+</template>
---
+++ pages/terminal/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="flex h-svh justify-center items-center bg-[#0a0a0a] text-[#33ff00]"
+  >
+    <div class="flex flex-col items-center gap-8">
+      <h1 class="font-bold">[ T Ö R M I N Ä L ]</h1>
+      <a href="./canvas/">[ Canvas ]</a>
+    </div>
+  </div>
+</template>
