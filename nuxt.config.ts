9795c0f618466f0629d925c54a85939ce81737d8
Josh Koroluk <jk@binaryone.ch>
2026-01-20 16:55:39 +0100

    feat: update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,20 @@ export default defineNuxtConfig({
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-    }
+    },
   },
   ssr: false,
+  app: {
+    head: {
+      title: 'wameguan',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: '96x96' },
+        { rel: 'shortcut icon', href: '/favicon.ico', sizes: '96x96' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '96x96' },
+        { rel: 'manifest', href: '/site.webmanifest', sizes: '96x96' },
+      ],
+      meta: [{ name: 'apple-mobile-web-app-title', content: 'korol.uk' }],
+    },
+  },
 });
